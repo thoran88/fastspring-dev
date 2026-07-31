83dc495a5e218d2bb4e70a8b926d3cfa3ef2ea38
thoran88 <thoran@fastspring.com>
2026-07-31 14:37:01 +0100

    removing the api integration to redo from screatch

--- Fastspring-API/src/fastspring/orders.ts
+++
@@ -1,6 +0,0 @@
-import { fastspring } from "./client.js";
-
-export async function getOrder(orderId: string) {
-  const { data } = await fastspring.get(`/orders/${orderId}`);
-  return data;
-}
--- Fastspring-API/src/fastspring/subscriptions.ts
+++
@@ -1,6 +0,0 @@
-import { fastspring } from "./client.js";
-
-export async function getSubscription(subscriptionId: string) {
-  const { data } = await fastspring.get(`/subscriptions/${subscriptionId}`);
-  return data;
-}
